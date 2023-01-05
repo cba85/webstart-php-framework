078580c820871fd099f469c4043f35cf228426f4
Clément Barbaza <clement.barbaza@gmail.com>
2023-01-05 11:06:29 +0100

    🎉 init

---
+++ README.md
@@ -0,0 +1,21 @@
+# Webstart PHP framework
+
+My personal framework
+
+## Usage
+
+```bash
+$ php -S 0.0.0.0:8080 -t public/
+# or
+$ composer run serve
+```
+
+## Features
+
+* `.env` file
+* Carbon for dates
+
+### Dev
+
+* Whoops error
+* Faker
---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__ . "/../vendor/autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
+$dotenv->load();
+
+$whoops = new \Whoops\Run;
+$whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+$whoops->register();
+
+echo "Hello world";
